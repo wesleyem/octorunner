7576bfcb70dbd2238c0408cb2c0d2d59be32e5dd
wesleyem <wesleyem@pm.me>
2025-01-14 13:52:52 -0800

    fix: remove dockerd, passing the docker socket is mandatory

--- start.sh
+++ start.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-dockerd &
-
-TIMEOUT=10
-SECONDS=0
-while ! docker info > /dev/null 2>&1; do
-    echo "Waiting for Docker to start..."
-    sleep 1
-    if [ $SECONDS -ge $TIMEOUT ]; then
-        echo "Docker failed to start within $TIMEOUT seconds."
-        exit 1
-    fi
-done
-
-echo "Docker is running!"
-
 if [ -z "$REPO" ] || [ -z "$TOKEN" ]; then
     echo "Error: REPO and TOKEN environment variables must be set."
     exit 1
@@ -24,7 +9,7 @@ REPOSITORY=$REPO
 ACCESS_TOKEN=$TOKEN
 
 echo "REPO: ${REPOSITORY}"
-echo "ACCESS_TOKEN: ${ACCESS_TOKEN}"
+echo "ACCESS_TOKEN: <hidden>"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" \
     https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
